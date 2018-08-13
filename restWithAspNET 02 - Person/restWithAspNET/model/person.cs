40e7b9d8a1e5cf96c3e45e26cb9f30c2d616b2a9
Eduardo Gracez Dusik <dudu.gracez@gmail.com>
2018-08-13 17:25:29 -0300

    'principalsVerbs'

---
+++ restWithAspNET 02 - Person/restWithAspNET/model/person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restWithAspNET.model
+{
+    public class person
+    {
+        public long id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string adress { get; set; }
+        public string gender { get; set; }
+    }
+}
